dd88f9863e4f9670437b749f0ee0b16af2e6bb8d
fangzheng <fangzheng@nrp-pet.cn>
2025-07-10 18:24:05 +0800

    Initial Commit

---
+++ ClipboardManager/ClipboardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardManagerApp.swift
+//  ClipboardManager
+//
+//  Created by zfang on 2025/7/10.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardManagerTests/ClipboardManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClipboardManagerTests.swift
+//  ClipboardManagerTests
+//
+//  Created by zfang on 2025/7/10.
+//
+
+import Testing
+
+struct ClipboardManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
